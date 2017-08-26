f34d6633a2436467b73d3653eab3acdc34421728
Jeffrey Leary <jeff@Jeffreys-MacBook-Air.local>
2017-08-26 08:58:54 -0400

    corrected wsgi file

--- conf/install.bash
+++ conf/install.bash
@@ -27,10 +27,18 @@ sudo pip install flask-wtf
 sudo pip install flask-sqlalchemy
 sudo pip install sqlalchemy-migrate
 
-# download greenery requirements
+# download greenery app
 sudo git clone https://github.com/jeffleary00/greenery.git /var/www/greenery
-sudo git clone https://github.com/toofishes/rfoutlet-pi.git /var/www/bin
 
+# build rfoutlet binary
+git clone https://github.com/toofishes/rfoutlet-pi.git /tmp/rfoutlet-pi
+mv /tmp/rfoutlet-pi/Makefile /tmp/rfoutlet-pi/Makefile.bak
+cp /var/www/greenery/conf/rfoutlet.Makefile /tmp/rfoutlet-pi/Makefile
+cd /tmp/rfoutlet-pi
+sudo make
+sudo -u www-data cp /tmp/rfoutlet-pi/rfoutlet /var/www/greenery/bin
+
+# configure web app
 sudo chown -R www-data /var/www 
 sudo chgrp -R www-data /var/www
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
@@ -38,8 +46,15 @@ sudo cp /var/www/greenery/conf/www/nginx.default /etc/nginx/sites-available/defa
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo cp /var/www/greenery/conf/www/uwsgi.ini /etc/uwsgi/apps-available/uwsgi.ini
 sudo ln -s /etc/uwsgi/apps-available/uwsgi.ini /etc/uwsgi/apps-enabled/uwsgi.ini
+
+# inital app db
 cd /var/www/greenery
-sudo -u www-data python ./db_create.py
+sudo -u www-data python db_create.py
+
+# restart services
+sudo service uwsgi restart
+sudo service nginx restart
+
 
 
 
